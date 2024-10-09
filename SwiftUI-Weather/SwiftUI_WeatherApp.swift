0ccef776dc36832080a1abf13b3003a8add5266b
Alex Gerus <alexander.gerus@grse.de>
2024-10-09 14:08:39 +0300

    Init

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Alexander Gerus on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
